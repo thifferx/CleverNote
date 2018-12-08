33fefdaa2928e9068e05cd8a8aeb65821eaff784
ThifferX <thifferx@gmail.com>
2018-12-08 23:58:28 +0100

    ActionBar Fragments

--- app/src/main/java/com/example/tom_pc/clevernote/MainActivity.java
+++ app/src/main/java/com/example/tom_pc/clevernote/MainActivity.java
@@ -6,39 +6,72 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new PostFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_text);
+        }
     }
 
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_text:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new PostFragment()).commit();
+                break;
+            case R.id.nav_audio:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AudioFragment()).commit();
+                break;
+            case R.id.nav_settings:
+                Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_person:
+                Toast.makeText(this, "Send", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
         return true;
     }
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.add:
-                Intent intent = new Intent(getApplicationContext(),AddNew.class);
-                startActivity(intent);
-                break;
-            case R.id.tabout:
-                Intent intent2 = new Intent(getApplicationContext(),About.class);
-                startActivity(intent2);
-                break;
-            case R.id.settings:
-                Intent intent3 = new Intent(getApplicationContext(),Settings.class);
-                startActivity(intent3);
-                break;
-            default:
-                return super.onOptionsItemSelected(item);
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
-        return true;
     }
 }
